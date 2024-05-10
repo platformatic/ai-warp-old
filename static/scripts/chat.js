e8416a13771c4697e41272bf22479e3de7132a39
flakey5 <73616808+flakey5@users.noreply.github.com>
2024-05-10 01:05:08 -0700

    Finish up frontend (#41)
    * Finish up frontend
    
    * fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    ---------
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    Co-authored-by: Matteo Collina <hello@matteocollina.com>

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,45 +1,29 @@
-// For temporarily disabling further prompts if we're already responding to one
-let isAlreadyResponding = false
+let promptLock = false
+const messages = []
+const messagesContainer = document.getElementById('messages')
 const promptInput = document.getElementById('prompt-input')
 const promptButton = document.getElementById('prompt-button')
-const messagesElement = document.getElementById('messages')
-
-/**
- * List of completed messages to easily keep track of them instead of making
- *  calls to the DOM
- *
- * { type: 'prompt' | 'response' | 'error', message?: string }
- */
-const messages = []
 
 promptButton.onclick = () => {
-  const prompt = promptInput.value
-  if (prompt === '' || isAlreadyResponding) {
-    return
-  }
-
-  createMessageElement('prompt', prompt)
-  promptAiWarp(prompt).catch(err => {
+  handlePrompt(promptInput.value).catch(err => {
     throw err
   })
-
-  promptInput.value = ''
 }
 
 /**
  * @param {KeyboardEvent} event
- */
+*/
 promptInput.onkeydown = (event) => {
-  if (event.key === 'Enter') {
-    promptButton.onclick()
+  if (event.key === 'Enter' && !event.shiftKey) {
+    handlePrompt(promptInput.value).catch(err => {
+      throw err
+    })
   }
 }
 
 const searchParams = new URL(document.location.toString()).searchParams
 if (searchParams.has('prompt')) {
-  const prompt = searchParams.get('prompt')
-  createMessageElement('prompt', prompt)
-  promptAiWarp(prompt).catch(err => {
+  handlePrompt(searchParams.get('prompt')).catch(err => {
     throw err
   })
 }
@@ -47,8 +31,34 @@ if (searchParams.has('prompt')) {
 /**
  * @param {string} prompt
  */
-async function promptAiWarp (prompt) {
-  isAlreadyResponding = true
+async function handlePrompt (prompt) {
+  if (prompt === '' || promptLock) {
+    return
+  }
+  promptInput.value = ''
+
+  const message = {
+    prompt,
+    response: [],
+    responseIndex: 0
+  }
+  messages.push(message)
+  drawMessage(message, true, false)
+
+  await promptAiWarp(message)
+  drawMessage(message, false, true)
+}
+
+/**
+ * @param {{
+ *  prompt: string,
+ *  response: ReadableStream[],
+ *  responseIndex: number,
+ *  errored: boolean
+ * }} message
+ */
+async function promptAiWarp (message) {
+  promptLock = true
 
   try {
     const res = await fetch('/api/v1/stream', {
@@ -56,110 +66,398 @@ async function promptAiWarp (prompt) {
       headers: {
         'content-type': 'application/json'
       },
-      body: JSON.stringify({ prompt })
+      // TODO chat history
+      body: JSON.stringify({ prompt: message.prompt })
     })
     if (res.status !== 200) {
       const { message, code } = await res.json()
       throw new Error(`AI Warp error: ${message} (${code})`)
     }
 
-    createMessageElement('response', res.body)
+    message.response[message.responseIndex] = res.body
   } catch (err) {
-    createMessageElement('error')
+    promptLock = false
+    message.errored = true
     console.error(err)
-    isAlreadyResponding = false
   }
 }
 
 /**
- * @param {'prompt' | 'response' | 'error'} type
- * @param {string | ReadableStream | undefined} message String if it's a prompt, ReadableStream if it's a response
+ * @param {{
+ *  prompt: string,
+ *  response: (string[] | ReadableStream)[],
+ *  responseIndex: number,
+ *  errored: boolean | undefined
+ * }} message
+ * @param {boolean} drawPrompt
+ * @param {boolean} drawResponse
  */
-function createMessageElement (type, message) {
-  const messageElement = document.createElement('div')
-  messageElement.classList.add('message')
-  messagesElement.appendChild(messageElement)
-
-  const avatarElement = document.createElement('div')
-  avatarElement.classList.add('message-avatar')
-  messageElement.appendChild(avatarElement)
-
-  const avatarImg = document.createElement('img')
-  avatarElement.appendChild(avatarImg)
-
-  const contentsElement = document.createElement('div')
-  contentsElement.classList.add('message-contents')
-  messageElement.appendChild(contentsElement)
-
-  const authorElement = document.createElement('p')
-  authorElement.classList.add('message-author')
-  contentsElement.appendChild(authorElement)
-
-  if (type === 'prompt') {
-    avatarImg.setAttribute('src', '/images/avatars/you.svg')
-    authorElement.innerHTML = 'You'
-  } else {
-    avatarImg.setAttribute('src', '/images/avatars/platformatic.svg')
-    authorElement.innerHTML = 'Platformatic Ai-Warp'
+function drawMessage (message, drawPrompt, drawResponse) {
+  if (drawPrompt) {
+    drawPromptMessage(message)
   }
-
-  if (type === 'error' && message === undefined) {
-    // Display error message
-    const textElement = document.createElement('p')
-    textElement.classList.add('message-error')
-    textElement.innerHTML = '<img src="/images/icons/error.svg" alt="Error" /> Something went wrong. If this issue persists please contact us at support@platformatic.dev'
-    contentsElement.appendChild(textElement)
-    textElement.scrollIntoView()
-
-    messages.push({ type: 'error' })
-  } else if (typeof message === 'string') {
-    // Echo prompt back to user
-    const textElement = document.createElement('p')
-    textElement.innerHTML = message
-    contentsElement.appendChild(textElement)
-    contentsElement.appendChild(createMessageOptionsElement('prompt', message))
-    textElement.scrollIntoView()
-
-    messages.push({ type: 'prompt', message })
-  } else {
-    // Parse response from api
-    parseResponse(contentsElement, message)
-      .then(() => {
-        isAlreadyResponding = false
-      })
-      .catch(err => {
-        createMessageElement('error')
-        console.error(err)
-      })
+  if (drawResponse) {
+    drawResponseMessage(message)
   }
 }
 
 /**
- * @param {string} response
+ * @param {{ prompt: string }} message
+ */
+function drawPromptMessage (message) {
+  const element = document.createElement('div')
+  element.classList.add('message')
+  element.appendChild(drawMessageAvatar('prompt'))
+  element.appendChild(drawMessageContents('prompt', message))
+
+  messagesContainer.appendChild(element)
+}
+
+/**
+ * @param {{
+ *  prompt: string,
+*   response: (string[] | ReadableStream)[],
+*   responseIndex: number,
+*   errored: boolean | undefined
+*  }} message
+ */
+function drawResponseMessage (message) {
+  const element = document.createElement('div')
+  element.classList.add('message')
+  element.appendChild(drawMessageAvatar('response'))
+  element.appendChild(drawMessageContents('response', message))
+
+  messagesContainer.appendChild(element)
+}
+
+/**
+ * @param {'prompt' | 'response'} type
+ * @returns {HTMLDivElement}
+ */
+function drawMessageAvatar (type) {
+  const element = document.createElement('div')
+  element.classList.add('message-avatar')
+
+  const img = document.createElement('img')
+  if (type === 'prompt') {
+    img.setAttribute('src', '/images/avatars/you.svg')
+    img.setAttribute('alt', 'You')
+  } else {
+    img.setAttribute('src', '/images/avatars/platformatic.svg')
+    img.setAttribute('alt', 'Platformatic Ai-Warp')
+  }
+  element.appendChild(img)
+
+  return element
+}
+
+/**
+ * @param {'prompt' | 'response'} type
+ * @param {{
+ *  prompt: string,
+ *  response: (string[] | ReadableStream)[],
+ *  responseIndex: number,
+ *  errored: boolean | undefined
+ * }} message
+ * @returns {HTMLDivElement}
+ */
+function drawMessageContents (type, message) {
+  const element = document.createElement('div')
+  element.classList.add('message-contents')
+
+  const author = document.createElement('p')
+  author.classList.add('message-author')
+  author.innerHTML = type === 'prompt' ? 'You' : 'Platformatic Ai-Warp'
+  element.appendChild(author)
+
+  if (message.errored) {
+    element.appendChild(drawErrorMessageContents(message))
+  } else if (type === 'prompt') {
+    element.appendChild(drawPromptMessageContents(type, message))
+    element.appendChild(drawPromptMessageOptions(message))
+  } else {
+    // ReadableStream doesn't have a length property
+    if (message.response[message.responseIndex].length !== undefined) {
+      drawCompletedResponseMessageContents(element, message)
+      element.appendChild(drawResponseMessageOptions(message))
+    } else {
+      drawStreamedMessageContents(element, message)
+        .then(() => element.appendChild(drawResponseMessageOptions(message)))
+        .catch(err => {
+          throw err
+        })
+    }
+  }
+
+  return element
+}
+
+/**
+ * @returns {HTMLParagraphElement}
+ */
+function drawErrorMessageContents () {
+  const element = document.createElement('p')
+  element.classList.add('message-error')
+  element.innerHTML = '<img src="/images/icons/error.svg" alt="Error" /> Something went wrong. If this issue persists please contact us at support@platformatic.dev'
+
+  return element
+}
+
+/**
+ * @param {'prompt' | 'response'} type
+ * @param {{ prompt: string }} message
+ * @returns {HTMLParagraphElement}
+ */
+function drawPromptMessageContents (type, message) {
+  const element = document.createElement('p')
+  element.appendChild(document.createTextNode(message.prompt))
+
+  return element
+}
+
+/**
+ * @param {HTMLDivElement} parent
+ * @param {{ prompt: string, response: string[][], responseIndex: number }} message
+ */
+function drawCompletedResponseMessageContents (parent, message) {
+  let i = 0
+  do {
+    const element = document.createElement('p')
+    element.appendChild(document.createTextNode(message.response[message.responseIndex][i]))
+    parent.appendChild(element)
+    i++
+  } while (i < message.response[message.responseIndex].length)
+}
+
+/**
+ * @param {HTMLDivElement} parent
+ * @param {{ prompt: string, response: ReadableStream, responseIndex: number }} message
+ */
+async function drawStreamedMessageContents (parent, message) {
+  let fullResponse = ''
+  let current = document.createElement('p')
+  let newLine = true
+  parent.appendChild(current)
+
+  const parser = new SSEParser(message.response[message.responseIndex])
+  while (true) {
+    const tokens = await parser.pull()
+    if (tokens === undefined) {
+      break
+    }
+
+    const tokenString = escapeHtml(tokens.join(''))
+    fullResponse += tokenString
+
+    const lines = tokenString.split('\n')
+    for (let i = 0; i < lines.length; i++) {
+      if (newLine) {
+        lines[i] = lines[i].replace(/^ +/g, (spaces) => {
+          return spaces.split('').map(() => '&nbsp;').join('')
+        })
+        newLine = false
+      }
+      current.innerHTML += lines[i]
+
+      if (i + 1 < lines.length) {
+        current = document.createElement('p')
+        parent.appendChild(current)
+        current.scrollIntoView()
+        newLine = true
+      }
+    }
+  }
+
+  // Not great, but works for now
+  message.response[message.responseIndex] = []
+  for (const line of fullResponse.split('\n')) {
+    message.response[message.responseIndex].push(line)
+  }
+
+  promptLock = false
+}
+
+/**
+ * @param {{ prompt: string, response: string, errored: boolean | undefined }} message
+ * @returns {HTMLParagraphElement}
+ */
+function drawPromptMessageOptions (message) {
+  const element = document.createElement('div')
+  element.classList.add('message-options')
+
+  const rightAlignedElements = document.createElement('div')
+  rightAlignedElements.classList.add('message-options-right')
+  rightAlignedElements.appendChild(drawEditPromptButton(element, message))
+  element.appendChild(rightAlignedElements)
+
+  return element
+}
+
+/**
+ * @param {HTMLDivElement} parent
+ * @param {{ prompt: string }} message
  * @returns {HTMLButtonElement}
  */
-function createCopyResponseButton (response) {
+function drawEditPromptButton (parent, message) {
   const element = document.createElement('button')
   element.onclick = () => {
-    navigator.clipboard.writeText(response)
+    // Set the prompt text to be editable
+    parent.parentNode.children.item(1).setAttribute('contenteditable', 'true')
+
+    parent.innerHTML = ''
+    parent.appendChild(drawCancelPromptEditButton(parent, message))
+    parent.appendChild(drawSubmitPromptEditButton(parent, message))
   }
 
   const icon = document.createElement('img')
-  icon.setAttribute('src', '/images/icons/copy.svg')
-  icon.setAttribute('alt', 'Copy')
+  icon.setAttribute('src', '/images/icons/edit.svg')
+  icon.setAttribute('alt', 'Edit')
   element.appendChild(icon)
 
   return element
 }
 
 /**
- * @param {string} response
+ * @param {HTMLDivElement} parent
+ * @param {{ prompt: string }} message
  * @returns {HTMLButtonElement}
  */
-function createRegenerateResponseButton (response) {
+function drawCancelPromptEditButton (parent, message) {
+  const element = document.createElement('button')
+  element.classList.add('cancel-prompt-edit-button')
+  element.innerHTML = 'Cancel'
+
+  element.onclick = () => {
+    const promptParagraph = parent.parentNode.children.item(1)
+    promptParagraph.setAttribute('contenteditable', false)
+    promptParagraph.innerHTML = message.prompt
+
+    parent.parentNode.appendChild(drawPromptMessageOptions(message))
+    parent.remove()
+  }
+
+  return element
+}
+
+/**
+ * @param {HTMLDivElement} parent
+ * @param {{ prompt: string }} message
+ * @returns {HTMLButtonElement}
+ */
+function drawSubmitPromptEditButton (parent, message) {
+  const element = document.createElement('button')
+  element.classList.add('submit-prompt-edit-button')
+  element.innerHTML = 'Save and submit'
+
+  element.onclick = () => {
+    if (promptLock) {
+      return
+    }
+
+    const promptParagraph = parent.parentNode.children.item(1)
+    const newPrompt = promptParagraph.innerHTML
+    if (newPrompt === message.prompt) {
+      // No change
+      return
+    }
+
+    message.prompt = newPrompt
+
+    promptAiWarp(message)
+      .catch(err => {
+        throw err
+      })
+      .finally(() => {
+        redrawMessages()
+      })
+  }
+
+  return element
+}
+
+/**
+ * @param {{ prompt: string, response: (string[] | ReadableStream)[], responseIndex: number }} message
+ * @returns {HTMLDivElement}
+ */
+function drawResponseMessageOptions (message) {
+  const element = document.createElement('div')
+  element.classList.add('message-options')
+
+  if (message.response.length > 1) {
+    element.appendChild(drawResponseIndexSelector(message))
+  }
+
+  const rightAlignedElements = document.createElement('div')
+  rightAlignedElements.classList.add('message-options-right')
+  rightAlignedElements.appendChild(drawRegenerateResponseButton(message))
+  rightAlignedElements.appendChild(drawCopyResponseButton(message))
+  element.appendChild(rightAlignedElements)
+
+  return element
+}
+
+/**
+ * @param {{ response: (string[] | ReadableStream)[], responseIndex: number }} message
+ * @returns {HTMLDivElement}
+ */
+function drawResponseIndexSelector (message) {
+  const element = document.createElement('div')
+  element.classList.add('response-index-selector')
+
+  const leftArrow = document.createElement('button')
+  const leftArrowIcon = document.createElement('img')
+  leftArrowIcon.setAttribute('src', '/images/icons/arrow-left.svg')
+  leftArrowIcon.setAttribute('alt', 'Previous')
+  leftArrow.appendChild(leftArrowIcon)
+  element.appendChild(leftArrow)
+
+  leftArrow.onclick = () => {
+    if (message.responseIndex === 0) {
+      return
+    }
+
+    message.responseIndex--
+    redrawMessages()
+  }
+
+  const positionText = document.createElement('p')
+  positionText.innerHTML = `${message.responseIndex + 1}/${message.response.length}`
+  element.appendChild(positionText)
+
+  const rightArrow = document.createElement('button')
+  const rightArrowIcon = document.createElement('img')
+  rightArrowIcon.setAttribute('src', '/images/icons/arrow-right.svg')
+  rightArrowIcon.setAttribute('alt', 'Next')
+  rightArrow.appendChild(rightArrowIcon)
+  element.appendChild(rightArrow)
+
+  rightArrow.onclick = () => {
+    if (message.responseIndex + 1 >= message.response.length) {
+      return
+    }
+
+    message.responseIndex++
+    redrawMessages()
+  }
+
+  return element
+}
+
+/**
+ * @param {{ prompt: string, responseIndex: number }} message
+ * @returns {HTMLButtonElement}
+ */
+function drawRegenerateResponseButton (message) {
   const element = document.createElement('button')
   element.onclick = () => {
-    // TODO
+    message.responseIndex++
+    promptAiWarp(message)
+      .catch(err => {
+        throw err
+      })
+      .finally(() => {
+        redrawMessages()
+      })
   }
 
   const icon = document.createElement('img')
@@ -171,68 +469,34 @@ function createRegenerateResponseButton (response) {
 }
 
 /**
- * @param {'prompt' | 'response'} type
- * @param {string} message
- * @param {number} messageIndex Index of the message in {@link messages}
- * @returns {HTMLDivElement}
+ * @param {{ response: string[], responseIndex: number }} message
+ * @returns {HTMLButtonElement}
  */
-function createMessageOptionsElement (type, message, messageIndex) {
-  const messageOptions = document.createElement('div')
-  messageOptions.classList.add('message-options')
+function drawCopyResponseButton (message) {
+  const element = document.createElement('button')
 
-  if (type === 'prompt') {
-    // TODO
-  } else if (type === 'response') {
-    messageOptions.appendChild(createRegenerateResponseButton(message))
-    messageOptions.appendChild(createCopyResponseButton(message))
+  const icon = document.createElement('img')
+  icon.setAttribute('src', '/images/icons/copy.svg')
+  icon.setAttribute('alt', 'Copy')
+  element.appendChild(icon)
+
+  element.onclick = () => {
+    navigator.clipboard.writeText(message.response[message.responseIndex])
+
+    icon.setAttribute('src', '/images/icons/checkmark.svg')
+    setTimeout(() => {
+      icon.setAttribute('src', '/images/icons/copy.svg')
+    }, 2000)
   }
 
-  return messageOptions
+  return element
 }
 
-/**
- * @param {HTMLDivElement} parentElement Parent
- * @param {ReadableStream} stream To read from
- */
-async function parseResponse (parentElement, stream) {
-  let isFirstPass = true
-
-  let currentElement = document.createElement('p')
-  currentElement.innerHTML = '<i>Platformatic Ai-Warp is typing...</i>'
-  parentElement.appendChild(currentElement)
-
-  let fullResponse = ''
-
-  const parser = new SSEParser(stream)
-  while (true) {
-    if (isFirstPass) {
-      currentElement.innerHTML = ''
-      isFirstPass = false
-    }
-
-    const tokens = await parser.pull()
-    if (tokens === undefined) {
-      break
-    }
-
-    const tokenString = tokens.join('')
-    fullResponse += tokenString
-
-    const lines = tokenString.split('\n')
-    for (let i = 0; i < lines.length; i++) {
-      currentElement.innerHTML += lines[i]
-
-      if (i + 1 < lines.length) {
-        currentElement = document.createElement('p')
-        parentElement.appendChild(currentElement)
-        currentElement.scrollIntoView()
-      }
-    }
+function redrawMessages () {
+  messagesContainer.innerHTML = ''
+  for (const message of messages) {
+    drawMessage(message, true, true)
   }
-
-  parentElement.appendChild(createMessageOptionsElement('response', fullResponse))
-
-  messages.push({ type: 'response', message: fullResponse })
 }
 
 /**
@@ -293,4 +557,18 @@ class SSEParser {
 
     return tokens
   }
+}
+
+function escapeHtml (str) {
+  return str.replace(
+    /[&<>'"]/g,
+    tag =>
+      ({
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        "'": '&#39;',
+        '"': '&quot;'
+      }[tag] || tag)
+  )
 }
